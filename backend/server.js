a7f2fc5393b981d5cd17677f1ee74d03097bd79c
jonher16KAIST <jonher16@kaist.ac.kr>
2022-11-18 12:09:53 +0900

    server file cleanup

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
 
 app.use(cors())
 app.use(express.json())
@@ -44,36 +42,17 @@ app.post('/api/login', async (req, res) => {
 	
 })
 
-app.get('/api/quote', async (req, res) => {
-	const token = req.headers['x-access-token']
-
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
 	try {
-		const decoded = jwt.verify(token, 'secret123')
-		const email = decoded.email
-		const user = await User.findOne({ email: email })
-
-		return res.json({ status: 'ok', quote: user.quote })
-	} catch (error) {
-		console.log(error)
-		res.json({ status: 'error', error: 'invalid token' })
-	}
-})
-
-app.post('/api/quote', async (req, res) => {
-	const token = req.headers['x-access-token']
-
-	try {
-		const decoded = jwt.verify(token, 'secret123')
-		const email = decoded.email
-		await User.updateOne(
-			{ email: email },
-			{ $set: { quote: req.body.quote } }
-		)
-
-		return res.json({ status: 'ok' })
-	} catch (error) {
-		console.log(error)
-		res.json({ status: 'error', error: 'invalid token' })
+		await User.create({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.pwd,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: err })
 	}
 })
 
